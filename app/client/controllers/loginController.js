60d7e1b522767d346e00d95750b1f17c59ac85dd
Kyle Buzby <kyle.buzby@gmail.com>
2017-07-21 23:10:35 -0500

    basic signup page and controller

--- app/client/controllers/loginController.js
+++ app/client/controllers/loginController.js
@@ -4,6 +4,8 @@ module.exports = function(app) {
     if (Session.user) goToPredictPage();
 
     $scope.login = function(username,password) {
+
+      //TODO make sure there's some validation
       User.login({username: username, password: password}).then(function(retData) {
         Session.login(retData.user);
         goToPredictPage();
--- app/client/controllers/signupController.js
+++ app/client/controllers/signupController.js
@@ -1,5 +1,23 @@
 module.exports = function(app) {
-  app.controller('signupController',[function() {
+  app.controller('signupController',['$window','Session','User',function($window,Session,User) {
 
+    if (Session.user) goToPredictPage();
+
+    $scope.signup = function(name,username,password,location) {
+      //TODO make sure there's some validation
+      User.signup({
+        name: name,
+        username: username,
+        password: password,
+        location: location
+      }).then(function(retData) {
+        Session.login(retData.user);
+        goToPredictPage();
+      })
+    }
+
+    function goToPredictPage() {
+      $window.location.href = "/predict";
+    }
   }]);
 }
