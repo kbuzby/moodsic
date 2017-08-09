de28e9b62b49bd59811038df163a263143a496da
Kyle Buzby <kyle.buzby@gmail.com>
2017-08-09 15:40:25 -0500

    fix

--- app/server/controllers/track.js
+++ app/server/controllers/track.js
@@ -110,7 +110,7 @@ function queryTracks(relatedArtists,mode,tempo,loudness,hotttness,res){
     });
   }
 
-  query.select('title album').limit('500');
+  query.select('title album').limit(500);
 
   query.exec(function(err,tracks){
     if (err) {
